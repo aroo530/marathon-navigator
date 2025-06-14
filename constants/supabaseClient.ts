b8c0b7bfd15f67dfb283ec31fae69804b0de655f
Arsany Milad <arsanymilad45@gmail.com>
2025-06-14 15:42:11 +0300

    chore: update eas.json for Android build type, add @types/react-native dependency in package.json, and adjust file permissions for image assets

--- constants/supabaseClient.ts
+++ constants/supabaseClient.ts
@@ -1,15 +1,33 @@
 // constants/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
+import * as SecureStore from 'expo-secure-store';
 // import { universalStorage } from './universalStorage';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    // storage: universalStorage,
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: false,
-  },
-});
+// storage.ts
+import { Platform } from 'react-native';
+
+const isWeb = Platform.OS === 'web';
+const secureStorage = {
+  getItem: SecureStore.getItemAsync,
+  setItem: SecureStore.setItemAsync,
+  removeItem: SecureStore.deleteItemAsync,
+};
+
+let auth: any = {
+  autoRefreshToken: true,
+  persistSession: true,
+}
+if (!isWeb) {
+  auth['storage'] = secureStorage;
+}
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    auth
+  }
+);
